eb067b6a0d5633a943c064894af17f35938961cb
saturn <maruu987654321@gmail.com>
2022-02-17 02:09:51 +0300

    Add complex search in app.py

--- app.py
+++ app.py
@@ -21,13 +21,14 @@ def paging():
     current_page = int(request.args.get('p', 1))
     l_page = all_items // count_item
     items_show = entities[(current_page - 1) * count_item:current_page * count_item]
-    return render_template("main.html", entities=items_show, all_items=all_items, l_page=l_page, current_page=current_page)
+    return render_template("main.html", entities=items_show, all_items=all_items, l_page=l_page,
+                           current_page=current_page)
 
 
 @app.route('/search')
 def search():
     model = request.args.get('model')
-    with open('entities.json') as f:
+    with open('entities.json', encoding='utf-8') as f:
         entities = json.load(f)
         response = []
         if not model:
@@ -36,6 +37,25 @@ def search():
             for e in entities:
                 if e["model"] == model:
                     response.append(e)
+            if len(response) == 0:
+                model_split = model.split(" ")
+                for e in entities:
+                    source_find = e["model"]
+                    if source_find.split(" ")[0] == model_split[0]:
+                        response.append(e)
+                if len(response) == 0 and " " in model:
+                    for e in entities:
+                        source_find = e["model"]
+                        if " " in source_find:
+                            if source_find.split(" ")[1] == model_split[1]:
+                                response.append(e)
+                else:
+                    for e in entities:
+                        source_find = e["model"]
+                        if " " in source_find:
+                            if source_find.split(" ")[1] == model:
+                                response.append(e)
+
         return render_template("search_ause.html", entities=response)
 
 
@@ -47,6 +67,7 @@ def card(eid: int):
             if ent["id"] == eid:
                 return render_template("card_full.html", entity=ent)
 
+
 @app.route('/card/<int:eid>/short')
 def card_short(eid: int):
     with open('entities.json', encoding="utf-8") as f:
@@ -54,7 +75,6 @@ def card_short(eid: int):
         for ent in entities:
             if ent["id"] == eid:
                 return render_template("short_card.html", entity=ent)
-
 
 
 if __name__ == '__main__':
